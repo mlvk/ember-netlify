763e51d2913862c7f2760a9c0c034da4484c94f3
Aram Zadikian <brancusi@gmail.com>
2016-09-10 20:15:13 -0700

    wip

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /* jshint node: true */
 'use strict';
 
+var fs = require('fs-extra');
+
 module.exports = {
-  name: 'ember-netlify'
+  name: 'ember-netlify',
+  outputReady: function(result) {
+    console.log(result);
+    fs.copySync(".netlifyconfig", "dist/_redirects", { clobber: true });
+  }
 };
